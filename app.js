0fc0e4fab666c7dceef33237b3bba20615b1e255
Ben Brizzi <brizzitime@gmail.com>
2016-09-20 22:36:31 -0400

    added paralax

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+(function() {
+  angular
+  .module("vanphan", [])
+  console.log("hi")
+}());
---
+++ public/js/directives.js
@@ -0,0 +1,57 @@
+(function() {
+  angular.module('vanphan')
+  .directive('home', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/home.html'
+    };
+  })
+  .directive('homeContent', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/home-content.html'
+    };
+  })
+  .directive('portfolio', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/portfolio.html'
+    };
+  })
+  .directive('portfolioContent', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/portfolio-content.html'
+    };
+  })
+  .directive('project1', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/project1.html'
+    };
+  })
+  .directive('project2', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/project2.html'
+    };
+  })
+  .directive('project3', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/project3.html'
+    };
+  })
+  .directive('contact', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/contact.html'
+    };
+  })
+  .directive('navbar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'public/templates/navbar.html'
+    };
+  })
+}());
